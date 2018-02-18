6459a9026cc0f872d9f6efc62334c18d3c46c176
Dale Corns <codefellow@gmail.com>
2018-02-17 20:49:15 -0800

    using basic state

--- burger/src/App.js
+++ burger/src/App.js
@@ -4,13 +4,21 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Tom', age: 27},
+      {name: 'Sarah', age: 47},
+      {name: 'Edward', age: 94},
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1>I be a React application</h1>
-        <Person name="Tom" age="27"/>
-        <Person name="Sarah" age="47">I like Tom. {12*4}</Person>
-        <Person name="Edward" age="94"/>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hello Im a child element</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }

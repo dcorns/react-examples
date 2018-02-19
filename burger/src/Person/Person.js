f7e789175c72252ba62a148febf09d088436a79c
Dale Corns <codefellow@gmail.com>
2018-02-19 13:22:25 -0800

    style component using import

--- burger/src/Person/Person.js
+++ burger/src/Person/Person.js
@@ -5,9 +5,10 @@
  * MIT Licensed
  */
 import React from 'react';
+import './Person.css';//works from web pack
 const person = (props) => {
   return (
-    <div onClick={props.click}>
+    <div onClick={props.click} className="Person">
     <p>I'm {props.name} and I am {props.age} years old!. {props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}/>
     </div>

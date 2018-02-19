d7a24177c2c102a7da6e60108a45051d7d10306d
Dale Corns <codefellow@gmail.com>
2018-02-19 13:30:00 -0800

    scoping styles inline within the render function

--- burger/src/App.js
+++ burger/src/App.js
@@ -34,10 +34,19 @@ class App extends Component {
     );
   };
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    };
     return (
       <div className="App">
         <h1>I be a React application</h1>
-        <button onClick = {() => this.switchNameHandler('Tom Cruse')}>Switch Name</button>
+        <button
+          style={style}
+          onClick = {() => this.switchNameHandler('Tom Cruse')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}

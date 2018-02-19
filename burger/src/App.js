f1b403f43f69296f4ab5d7e924f405f6b1838884
Dale Corns <codefellow@gmail.com>
2018-02-19 12:44:18 -0800

    use function wrapping to pass args from render to class method

--- burger/src/App.js
+++ burger/src/App.js
@@ -26,14 +26,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1>I be a React application</h1>
-        <button onClick = {this.switchNameHandler.bind(this, 'Tom Cruse')}>Switch Name</button>
+        <button onClick = {() => this.switchNameHandler('Tom Cruse')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}/>
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-        click={this.switchNameHandler.bind(this,'Tom Hanks')}>
+        click={() => this.switchNameHandler('Using bind is more efficient!')}>
           Hello Im a child element
         </Person>
         <Person

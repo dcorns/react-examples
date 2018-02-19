b0de5a3a65b6cae886d1d362808bee3742c63140
Dale Corns <codefellow@gmail.com>
2018-02-19 15:00:59 -0800

    conditionally display content within render function outside of jsx

--- burger/src/App.js
+++ burger/src/App.js
@@ -47,14 +47,10 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer',
     };
-    return (
-      <div className="App">
-        <h1>I be a React application</h1>
-        <button
-          style={style}
-          onClick = {this.togglePersonsHandler}>Toggle Persons</button>
-        {this.state.showPersons &&
-          <div>
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
           <Person
             name={this.state.persons[0].name}
             age={this.state.persons[0].age}
@@ -68,10 +64,20 @@ class App extends Component {
           <Person
             name={this.state.persons[2].name}
             age={this.state.persons[2].age}/>
-        </div>}
+        </div>
+      );
+    }
+    return (
+      <div className="App">
+        <h1>I be a React application</h1>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons
+        </button>
+        {persons}
       </div>
     );
-  }
+  };
 }
 
 export default App;
